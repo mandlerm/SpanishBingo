052bfdf9368e29fe92ef6418498bda10d77455aa
mandlerm <amaya.melissa@gmail.com>
2018-04-05 06:29:05 -0500

    added rake tasks

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20180405015748) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "difficulties", force: :cascade do |t|
     t.string "level"
     t.datetime "created_at", null: false
