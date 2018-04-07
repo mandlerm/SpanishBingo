59b338c8265aac8dfd212f1a54b4b479e77920a5
mandlerm <amaya.melissa@gmail.com>
2018-04-07 12:35:09 -0500

    saving before creating bundle

--- client/src/App.js
+++ client/src/App.js
@@ -3,36 +3,12 @@ import logo from "./logo.svg"
 import "./App.css"
 
 class App extends Component {
-  constructor() {
-    super()
-
-    fetch(`/api/games`)
-      .then(function(response) {
-        return response.json()
-      })
-      .then(function(myJson) {
-        console.log(myJson)
-      })
-
-    let sample = "sample"
-    console.log("cards")
-    fetch(`/api/cards`, {
-      category: "sample"
-    })
-      .then(function(response) {
-        return response.json()
-      })
-      .then(function(myJson) {
-        console.log(myJson)
-      })
-  }
-
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Welcome to Spanish Bingo</h1>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
