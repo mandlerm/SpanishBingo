2e083d6e5c0a8435171f2fdefe4189394da10f74
mandlerm <amaya.melissa@gmail.com>
2018-04-13 11:27:47 -0500

    remove store directory. store created in index.js

---
+++ client/src/reducers/game_cards.js
@@ -0,0 +1,11 @@
+// export default function gameCards(
+//   state = { category: "sample" },
+//   action
+// ) {
+//   switch (action.type) {
+//     case SET_GAME_CARDS:
+//       return setGameCategory(state, action)
+//     default:
+//       return state
+//   }
+// }
--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,9 +1,12 @@
 import { combineReducers } from "redux"
 import gameSettingsReducer from "./game_setting_reducer"
 import levelReducer from "./level_selection"
+//import each reducer (need one reducer for each piece of data/state)
+//1. game category  2. game level  3. game cards  4. active word  5. score  6. time
 
 const rootReducer = combineReducers({
   game: gameSettingsReducer
+  level: levelReducer
 })
 
 export default rootReducer
