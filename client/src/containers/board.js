5be8696080221b0123c7dc580ff9d7a8f7551ca2
mandlerm <amaya.melissa@gmail.com>
2018-04-16 21:09:27 -0500

    timer mostly working. need a way to stop game when timer is up

--- client/src/containers/board.js
+++ client/src/containers/board.js
@@ -6,20 +6,6 @@ import { currentGameBoard } from "../actions/actions"
 
 //***** How do I update state with current board????***********//
 class Board extends React.Component {
-  // componentWillMount() {
-  //   let shuffledGameArray = this.props.state.cards.cards.sort(function(a, b) {
-  //     return 0.5 - Math.random()
-  //   })
-  //
-  //   //going to return 9 cards for easy game, 16 cards for standard game.
-  //   let leveledBoard = shuffledGameArray.slice(
-  //     0,
-  //     this.props.state.level.cardNumber
-  //   )
-  //   this.props.currentGameBoard(leveledBoard)
-  //
-  // }
-
   renderBoard() {
     if (!this.props.state.cards.cards) {
       return "loading"
--- client/src/containers/timer.js
+++ client/src/containers/timer.js
@@ -2,23 +2,59 @@ import React from "react"
 import { connect } from "react-redux"
 // import "./timer.css"
 import { bindActionCreators } from "redux"
+import Countdown from "react-countdown-now"
 // import { fetchLevels, setLevel } from "../actions/actions"
 //toggle levels
+const Completionist = () => <span>Need to figure this out</span>
 
-export default class Timer extends React.Component {
+const renderer = ({ hours, minutes, seconds, completed }) => {
+  if (completed) {
+    // Render a complete state
+    return <Completionist />
+  } else {
+    // Render a countdown
+    return (
+      <span>
+        {hours}:{minutes}:{seconds}
+      </span>
+    )
+  }
+}
+
+class Timer extends React.Component {
+  renderTimer() {
+    let timer = this.props.level * 1000
+
+    return (
+      <Countdown
+        className="clock"
+        date={Date.now() + timer}
+        intervalDelay={500}
+        precision={0}
+        renderer={renderer}
+      />
+    )
+  }
+
   render() {
-    return <div className="timer">I am a timer</div>
+    // console.log("state", this.props.state)
+    return (
+      <div className="timer">
+        Timer: <br />
+        {this.renderTimer()}
+      </div>
+    )
   }
 }
 //
-// function mapStateToProps(state) {
-//   return {
-//     level: state.level.levels
-//   }
-// }
+function mapStateToProps(state) {
+  return {
+    level: state.level.time
+  }
+}
 //
 // function mapDispatchToProps(dispatch) {
 //   return bindActionCreators({ fetchLevels, setLevel }, dispatch)
 // }
 //
-// export default connect(mapStateToProps, mapDispatchToProps)(Level)
+export default connect(mapStateToProps)(Timer)
