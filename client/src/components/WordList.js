8cd5b945c4f4e14b0850f15ff789b35e526d5748
mandlerm <amaya.melissa@gmail.com>
2018-04-21 16:45:27 -0500

    wordList component setup

--- client/src/components/WordList.js
+++ client/src/components/WordList.js
@@ -1,5 +1,27 @@
 import React from "react"
+import { bindActionCreators } from "redux"
+import { connect } from "react-redux"
 
-const WordList = () => <h1>I will be a list of words for each category</h1>
+import { currentGameBoard, playWord, setWordArray } from "../actions/actions"
 
-export default WordList
+class WordList extends React.Component {
+  render() {
+    return <h1>I will be a list of words for each category</h1>
+  }
+}
+
+//
+// function mapStateToProps(state) {
+//   return {
+//     state: state
+//   }
+// }
+
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators(
+    { currentGameBoard, playWord, setWordArray },
+    dispatch
+  )
+}
+
+export default connect(null, mapDispatchToProps)(WordList)
