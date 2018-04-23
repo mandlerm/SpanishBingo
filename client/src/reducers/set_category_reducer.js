2960aeca6c00731ea434a813a6764be70b8dae15
mandlerm <amaya.melissa@gmail.com>
2018-04-22 21:48:46 -0500

    minor changes cleaning up code before making a state name change

--- client/src/reducers/set_category_reducer.js
+++ client/src/reducers/set_category_reducer.js
@@ -1,12 +1,11 @@
 export default function(
-  state = { loading: false, cards: [], score: 0, cardNumber: 16 },
+  state = { loading: false, cards: [], cardNumber: 16 },
   action
 ) {
   switch (action.type) {
     case "SET_CATEGORY":
       return {
         ...state,
-        score: 0,
         cards: action.payload.cards,
         loading: false
       }
